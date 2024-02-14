3bec97f38a87d506095570bd1bc958bea83c4e10
slavarodionov <roval911@gmail.com>
2024-02-14 15:04:02 +0300

    подключил роутеры

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from dish.router import router as dish_router
+from menu.router import router as menu_router
+from submenu.router import router as submenu_router
+from users.router import router as users_router
 
 app = FastAPI()
 
+app.include_router(menu_router)
+app.include_router(submenu_router)
+app.include_router(dish_router)
+app.include_router(users_router)
 
 @app.get("/")
 async def root():
---
+++ users/router.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(
+    prefix="/Users",
+    tags=["Поьзователи"],
+)
